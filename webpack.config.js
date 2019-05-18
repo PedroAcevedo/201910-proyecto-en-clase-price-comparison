cf99433ce08d6cd66cd02bd0e108839e2b8af936
PedroAcevedo <pdacevedo@uninorte.edu.co>
2019-05-17 21:35:39 -0500

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+const fs = require("fs-extra");
+
+//copyPublicFolder
+fs.copySync(
+  path.resolve(__dirname, "public"),
+  path.resolve(__dirname, "dist"),
+  {
+    dereference: true
+  }
+);
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: '/'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 5000,
+    compress: true,
+    open: true,
+    historyApiFallback: true
+  },
+  mode: "development",
+  module: {
+    rules: [
+      { 
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" }
+        ]
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: ["@babel/plugin-proposal-class-properties"]
+          }
+        }
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: "html-loader",
+          options: {}
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|ico|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "images"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new Dotenv({
+      path: "./.env",
+      safe: false
+    })
+  ]
+};
