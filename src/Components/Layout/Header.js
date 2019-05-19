886f4c893444ed24ec46ccf3f1ae23d71032f060
Dino Perez <dperezhincapie@gmail.com>
2019-05-19 18:06:26 -0500

    prueba branch v2

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,122 +1,121 @@
 import React, { Component } from "react";
-import { AuthConsumer } from './../AuthContext'
+import { AuthConsumer } from "./../AuthContext";
 import { Link } from "react-router-dom";
 
 class Header extends Component {
-  constructor () {
-    super()
+  constructor() {
+    super();
   }
-  logout(e,logout){
-    logout()
+  logout(e, logout) {
+    logout();
   }
-  
+
   render() {
     return (
       <AuthConsumer>
-        {({isAuth,logout}) => (
-           <header className="header container">
-           <div className="container-flex-v">
-            { isAuth?
-              <div className="loginsButtons">
-                <a className="nav-link" onClick={(e)=>this.logout(e,logout)}>
-                  logout
-                </a>
-              </div>                
-              :
-              <div className="loginsButtons">
-               <Link to="/signin" className="nav-link">
-                 Signin
-               </Link>
-               <a>/</a>
-               <Link to="/signup" className="nav-link">
-                 Signup
-               </Link>
-             </div>            
-            }    
-             <div className="logo">
-               <Link to="/" className="nav-link">
-                 <img
-                   className="logo-img"
-                   src={require("../../Images/logo.png")}
-                   alt="sacrappy-logo"
-                   href="/"
-                 />
-               </Link>
-             </div>
-   
-            
-              { isAuth?
-                <nav className="nav-bar">  
+        {({ isAuth, logout }) => (
+          <header className="header container">
+            <div className="container-flex-v">
+              {isAuth ? (
+                <div className="loginsButtons">
+                  <a className="nav-link" onClick={e => this.logout(e, logout)}>
+                    logout
+                  </a>
+                </div>
+              ) : (
+                <div className="loginsButtons">
+                  <Link to="/signin" className="nav-link">
+                    Signin
+                  </Link>
+                  <a>/</a>
+                  <Link to="/signup" className="nav-link">
+                    Signup
+                  </Link>
+                </div>
+              )}
+              <div className="logo">
+                <Link to="/" className="nav-link">
+                  <img
+                    className="logo-img"
+                    src={require("../../Images/logo.png")}
+                    alt="sacrappy-logo"
+                    href="/"
+                  />
+                </Link>
+              </div>
+
+              {isAuth ? (
+                <nav className="nav-bar">
                   <Link to="/" className="nav-item">
                     Catálogo
                   </Link>
                   <Link to="/profile" className="nav-item">
-                    Perfil
+                    Perfil de usuario
                   </Link>
                   <Link to="/cart" className="nav-item">
                     Carrito
                   </Link>
                   <Link to="/history" className="nav-item">
                     Historial
-                  </Link> 
+                  </Link>
                   <div>
-                 <form className="form search-form">
-                   <div>
-                     <label for="search">
-                       <input
-                         type="button"
-                         className="input input-rounded"
-                         type="text"
-                         id="search"
-                         name="search"
-                         title="Search"
-                       />
-                     </label>
-                   </div>
-                   <div>
-                     <button className="button button-primary search-button">
-                       <img
-                         className="search-button-icon"
-                         src={require("../../CSS/icons/PNG/search.png")}
-                       />
-                     </button>
-                   </div>
-                 </form>
-               </div>
-             </nav>
-            :
-              <nav className="nav-bar">  
-                <Link to="/" className="nav-item">
-                  Catálogo
-                </Link>
-                <div>
-               <form className="form search-form">
-                 <div>
-                   <label for="search">
-                     <input
-                       type="button"
-                       className="input input-rounded"
-                       type="text"
-                       id="search"
-                       name="search"
-                       title="Search"
-                     />
-                   </label>
-                 </div>
-                 <div>
-                   <button className="button button-primary search-button">
-                     <img
-                       className="search-button-icon"
-                       src={require("../../CSS/icons/PNG/search.png")}
-                     />
-                   </button>
-                 </div>
-               </form>
-             </div>
-           </nav>
-          }
-           </div>
-         </header>
+                    <form className="form search-form">
+                      <div>
+                        <label for="search">
+                          <input
+                            type="button"
+                            className="input input-rounded"
+                            type="text"
+                            id="search"
+                            name="search"
+                            title="Search"
+                          />
+                        </label>
+                      </div>
+                      <div>
+                        <button className="button button-primary search-button">
+                          <img
+                            className="search-button-icon"
+                            src={require("../../CSS/icons/PNG/search.png")}
+                          />
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </nav>
+              ) : (
+                <nav className="nav-bar">
+                  <Link to="/" className="nav-item">
+                    Catálogo
+                  </Link>
+                  <div>
+                    <form className="form search-form">
+                      <div>
+                        <label for="search">
+                          <input
+                            type="button"
+                            className="input input-rounded"
+                            type="text"
+                            id="search"
+                            name="search"
+                            title="Search"
+                          />
+                        </label>
+                      </div>
+                      <div>
+                        <button className="button button-primary search-button">
+                          <img
+                            className="search-button-icon"
+                            src={require("../../CSS/icons/PNG/search.png")}
+                          />
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </nav>
+              )}
+            </div>
+          </header>
         )}
       </AuthConsumer>
     );
