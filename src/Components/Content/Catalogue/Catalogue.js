1597102a3121f7122586a75985838f8bd9c9e3cc
Dino Perez <dperezhincapie@gmail.com>
2019-05-22 15:52:23 -0500

    Estilos de botones Product

--- src/Components/Content/Catalogue/Catalogue.js
+++ src/Components/Content/Catalogue/Catalogue.js
@@ -2,13 +2,11 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Product from "./Product";
 import { Pagination } from "react-bootstrap";
-import { AuthConsumer} from './../../AuthContext';
+import { AuthConsumer } from "./../../AuthContext";
 class Catalogue extends Component {
-  
-  constructor(props){
-    super(props)	
+  constructor(props) {
+    super(props);
   }
-   
 
   render() {
     let active = 2;
@@ -24,34 +22,33 @@ class Catalogue extends Component {
     const paginationBasic = <Pagination size="sm">{items}</Pagination>;
     return (
       <AuthConsumer>
-      {({isAuth,user}) => (
-        <section className="catalogue">
-          <div className="catalogue-items item-list">
-     
-            {this.props.products.length > 0 ? (
-                    this.props.products.map(product => {
-                      return (
-                        <Product
-                        name={product.name}
-                        mark={product.brand}
-                        shop={product.chain}
-                        price={product.price}
-                        userId = {
-                          user[0] != null? user[0]._id : null
-                        }
-                        path={product.image}
-                        button = {isAuth}
-                      />
-                      );
-                    })
-                  ) : (
-                    <p>No hay productos registrados</p>
-            )}
-          </div>
-          {paginationBasic}
-        </section>
-      )}
-    </AuthConsumer>
+        {({ isAuth, user }) => (
+          <section className="catalogue">
+            <div className="catalogue-items item-list">
+              {this.props.products.length > 0 ? (
+                this.props.products.map(product => {
+                  return (
+                    <Product
+                      name={product.name}
+                      mark={product.brand}
+                      shop={product.chain}
+                      price={product.price}
+                      userId={user[0] != null ? user[0]._id : null}
+                      path={product.image}
+                      btn="btn-card-plus"
+                      type="Añadir"
+                      button={isAuth}
+                    />
+                  );
+                })
+              ) : (
+                <p>No hay productos registrados</p>
+              )}
+            </div>
+            {paginationBasic}
+          </section>
+        )}
+      </AuthConsumer>
     );
   }
 }
