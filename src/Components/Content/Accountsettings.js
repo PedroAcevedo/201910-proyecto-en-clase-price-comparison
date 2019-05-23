1ae8fb757b90f97a9a564f254389b8e05847b7fa
Dino Perez <dperezhincapie@gmail.com>
2019-05-23 09:42:25 -0500

    Registro

--- src/Components/Content/Accountsettings.js
+++ src/Components/Content/Accountsettings.js
@@ -1,172 +1,165 @@
 import React, { Component } from "react";
-import {profile,update} from './../../Services/api'
-import {NotificationContainer, NotificationManager} from 'react-notifications';
-import {AuthConsumer} from './../AuthContext';
-import { Redirect } from 'react-router';
-import Compress from 'compress.js'
-const compress = new Compress()
+import { profile, update } from "./../../Services/api";
+import {
+  NotificationContainer,
+  NotificationManager
+} from "react-notifications";
+import { AuthConsumer } from "./../AuthContext";
+import { Redirect } from "react-router";
+import Compress from "compress.js";
+const compress = new Compress();
 class Accountsettings extends Component {
-  
-  constructor(){
-    super()
-    this.state={
-      id:'',
-      email:'',
-      name:'',
-      address:'',
-      profileImage:''
-    }
+  constructor() {
+    super();
+    this.state = {
+      id: "",
+      email: "",
+      name: "",
+      address: "",
+      profileImage: ""
+    };
 
-    
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleFile = this.handleFile.bind(this);
   }
-  
 
-  handleChange(e){
-		this.setState({
-			[e.target.name]:e.target.value
-		})
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
   }
 
-  handleFile(e){
-    e.persist()
-    let reader = new FileReader()
-    const files = [...e.target.files]
-    compress.compress(files, {
-      size: 4, 
-      quality: .7, 
-      maxWidth: 500, 
-      maxHeight: 500, 
-      resize: true, 
-    })
-    .then((data) => {
-      let img = data[0]
-      reader.readAsDataURL(Compress.convertBase64ToFile(img.data,img.ext))
-      reader.onloadend = () => {
-        this.setState({
-          profileImage:reader.result
-        })
-      }
-    })  	
-    
+  handleFile(e) {
+    e.persist();
+    let reader = new FileReader();
+    const files = [...e.target.files];
+    compress
+      .compress(files, {
+        size: 4,
+        quality: 0.7,
+        maxWidth: 500,
+        maxHeight: 500,
+        resize: true
+      })
+      .then(data => {
+        let img = data[0];
+        reader.readAsDataURL(Compress.convertBase64ToFile(img.data, img.ext));
+        reader.onloadend = () => {
+          this.setState({
+            profileImage: reader.result
+          });
+        };
+      });
   }
 
-handleSubmit(e){
-    e.preventDefault()
+  handleSubmit(e) {
+    e.preventDefault();
     update({
       email: this.state.email,
       name: this.state.name,
       profileImage: this.state.profileImage,
       address: this.state.address
     })
-    .then(()=>{
-      NotificationManager.success('Datos actualizados', 'Scrappy')
-      this.props.history.push("/Signin");
-    })
-    .catch(error=>{
-			console.log(error.message)
-			NotificationManager.error('Error message', 'Ocurred an error')
-    })
+      .then(() => {
+        NotificationManager.success("Datos actualizados", "Scrappy");
+        this.props.history.push("/Signin");
+      })
+      .catch(error => {
+        console.log(error.message);
+        NotificationManager.error("Error message", "Ocurred an error");
+      });
   }
 
-  componentWillMount(){
-    profile("users",this.props.email)
-    .then( response => {
-      return response.json();
-    })
-    .then( json => {
-      this.setState({
-        id: json["_id"],
-        name: json["name"],
-        email: json["email"],
-        address: json["address"],
-        profileImage: json["profileImage"]
+  componentWillMount() {
+    profile("users", this.props.email)
+      .then(response => {
+        return response.json();
       })
-    })
+      .then(json => {
+        this.setState({
+          id: json["_id"],
+          name: json["name"],
+          email: json["email"],
+          address: json["address"],
+          profileImage: json["profileImage"]
+        });
+      });
   }
 
   render() {
     return (
       <AuthConsumer>
-        {({isAuth})=>(
-          isAuth?
-          <article className="accountsettings">
-          <div>
-          <NotificationContainer/>
-          </div>
-          {
-            this.state.email &&
-          <div>
-           <h2 className="title">Configuración de cuenta</h2>
-            <div className="container-flex-h">
+        {({ isAuth }) =>
+          isAuth ? (
+            <div className="accountsettings">
               <div>
-                <form className="form" onSubmit={this.handleSubmit}>
-                  <div>
-                    <label for="name" className="label">
-                      Nombre
+                <NotificationContainer />
+              </div>
+              {this.state.email && (
+                <div>
+                  <h2 className="title">Configuración de cuenta</h2>
+                  <div className="container-flex-v">
+                    <label htmlFor="profile-image" className="profile-image">
+                      <div className="profile-photo ">
+                        <img
+                          className="profile-photo"
+                          src={this.state.profileImage}
+                        />
+                        <i class="fas fa-image" />
+                      </div>
                     </label>
                     <input
-                      type="text"
-                      className="input"
-                      placeholder={this.state.name}
-                      id="name"
-                      name="name"
-                      title="Name"
-                      onChange = {this.handleChange}
+                      name="profile-image"
+                      id="profile-image"
+                      className="hidden"
+                      type="file"
+                      accept="image/*"
+                      onChange={this.handleFile}
                     />
+                    <div>
+                      <form className="form" onSubmit={this.handleSubmit}>
+                        <div>
+                          <label for="name" className="label">
+                            Nombre
+                          </label>
+                          <input
+                            type="text"
+                            className="input"
+                            placeholder={this.state.name}
+                            id="name"
+                            name="name"
+                            title="Name"
+                            onChange={this.handleChange}
+                          />
+                        </div>
+                        <div>
+                          <label for="address" className="label">
+                            Dirección
+                          </label>
+                          <input
+                            type="text"
+                            className="input"
+                            placeholder={this.state.address}
+                            id="address"
+                            name="address"
+                            title="address"
+                            onChange={this.handleChange}
+                          />
+                        </div>
+                        <button className="btn-profile">Actualizar</button>
+                      </form>
+                    </div>
                   </div>
-                  <div>
-                  <label for="address" className="label">
-                      Dirección
-                  </label>
-                    <input
-                      type="text"
-                      className="input"
-                      placeholder={this.state.address}
-                      id="address"
-                      name="address"
-                      title="address"
-                      onChange = {this.handleChange}
-                    />
-                  </div>
-                    <button className="button button-primary ">
-                      Actualizar
-                    </button>
-                </form>
-              </div>
-          <label htmlFor="profile-image">
-              <div className="profile-photo ">
-                <img
-                  className="profile-photo"
-                  src={this.state.profileImage}
-                />
-                 <span>
-                 <img
-                  src={require("../../CSS/icons/PNG/pencil.png")}
-                />
-                </span>
-              </div>
-            </label>
-            <input 
-                  name="profile-image" 
-                  id="profile-image" 
-                  className="hidden"
-                  type="file"
-                  accept="image/*"
-                  onChange={this.handleFile}
-            />
+                </div>
+              )}
             </div>
-            </div>
-          }
-        </article>
-      :
-          <Redirect to="/" />
-      )}
+          ) : (
+            <Redirect to="/" />
+          )
+        }
       </AuthConsumer>
     );
   }
 }
 export default Accountsettings;
-
--- src/Components/Content/Signin.js
+++ src/Components/Content/Signin.js
@@ -1,123 +1,129 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Redirect } from 'react-router';
-import { AuthConsumer } from './../AuthContext';
-import {signIn} from './../../Services/api';
-import {NotificationManager,NotificationContainer } from "react-notifications";
+import { Redirect } from "react-router";
+import { AuthConsumer } from "./../AuthContext";
+import { signIn } from "./../../Services/api";
+import {
+  NotificationManager,
+  NotificationContainer
+} from "react-notifications";
 
 class Signin extends Component {
-  
-  constructor(){
-		super()
-		this.state = {
-			email: '',
-      password:'',
-      admin: ''
-		}
-		this.handleChange = this.handleChange.bind(this)
-	}
-  
-  handleSubmit(e,login,isAuth){
-    e.preventDefault()
-  	signIn(this.state.email,this.state.password)
-    .then((response)=>{
-       return response.json();
-
-    })
-    .then((json) => {
-      if (json["login"]==true)
-      {
-        localStorage.setItem("user", this.state.email)
-        localStorage.setItem("token", json["token"])
-        if(json["admin"]==true){
-          this.setState({
-            admin: true
-          })
-        }
-        login(this.state.email)
-        NotificationManager.success('Ingreso exitoso', 'Scrappy')
-      }else{
-        NotificationManager.error('Usuario o contraseña incorrectas', 'Scrappy')
-      }
-    })
-		.catch(error=>{
-			console.log(error.message)
-			NotificationManager.error('Error message', 'Ocurred an error')
-    })
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      admin: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
-  
-  
-  handleChange(e){
-		this.setState({
-			[e.target.name]: e.target.value
-		  })
+
+  handleSubmit(e, login, isAuth) {
+    e.preventDefault();
+    signIn(this.state.email, this.state.password)
+      .then(response => {
+        return response.json();
+      })
+      .then(json => {
+        if (json["login"] == true) {
+          localStorage.setItem("user", this.state.email);
+          localStorage.setItem("token", json["token"]);
+          if (json["admin"] == true) {
+            this.setState({
+              admin: true
+            });
+          }
+          login(this.state.email);
+          NotificationManager.success("Ingreso exitoso", "Scrappy");
+        } else {
+          NotificationManager.error(
+            "Usuario o contraseña incorrectas",
+            "Scrappy"
+          );
+        }
+      })
+      .catch(error => {
+        console.log(error.message);
+        NotificationManager.error("Error message", "Ocurred an error");
+      });
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
   }
 
   render() {
     return (
       <AuthConsumer>
-          {({ login,isAuth}) => (
-                                    isAuth?
-                                      <div>
-                                        {
-                                          this.state.admin?
-                                          <Redirect to="/Adminprofile"/>
-                                          :
-                                          <Redirect to="/"/>
-                                        }  
-                                      </div>            
-                                    :
-                                    <article className="signin">
-                                      <form className="form" onSubmit={(e)=>this.handleSubmit(e,login,isAuth)}>
-                                        <h2 className="title">Sign In</h2>
-                                        <div>
-                                          <label for="email" className="label">
-                                            Correo
-                                          </label>
-                                          <input
-                                            type="text"
-                                          className="input"
-                                            placeholder="email"
-                                            id="email"
-                                            name="email"
-                                            title="Email"
-                                            value={this.state.email}
-                                            required
-                                            onChange={this.handleChange}
-                                          />
-                                        </div>
-                                        <div>
-                                          <label for="password" className="label">
-                                            Password
-                                          </label>
-                                          <input
-                                            type="password"
-                                            className="input"
-                                            placeholder="Password"
-                                            id="password"
-                                            name="password"
-                                            title="Password"
-                                            value={this.state.password}
-                                            required
-                                            onChange={this.handleChange}
-                                          />
-                                        </div>
-                                        <div>
-                                          <button className="button button-primary button-large">
-                                            Sign In
-                                          </button>
-                                        </div>
-                                        <div>
-                                          <a>
-                                            ¿Aun no tienes cuenta? <Link to="/signup">Registrate</Link>
-                                          </a>
-                                        </div>
-                                      </form>
-                                      <div>
-                                        <NotificationContainer/>
-                                      </div>
-                                  </article>
-          )}
+        {({ login, isAuth }) =>
+          isAuth ? (
+            <div>
+              {this.state.admin ? (
+                <Redirect to="/Adminprofile" />
+              ) : (
+                <Redirect to="/" />
+              )}
+            </div>
+          ) : (
+            <div className="sign">
+              <form
+                className="form"
+                onSubmit={e => this.handleSubmit(e, login, isAuth)}
+              >
+                <h2 className="title">Sign In</h2>
+                <div>
+                  <label for="email" className="label">
+                    Correo
+                  </label>
+                  <input
+                    type="text"
+                    className="input"
+                    placeholder="email"
+                    id="email"
+                    name="email"
+                    title="Email"
+                    value={this.state.email}
+                    required
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div>
+                  <label for="password" className="label">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className="input"
+                    placeholder="Password"
+                    id="password"
+                    name="password"
+                    title="Password"
+                    value={this.state.password}
+                    required
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div>
+                  <button className="btn-profile">Sign In</button>
+                </div>
+                <div>
+                  <a>
+                    ¿Aun no tienes cuenta?{" "}
+                    <Link className="link" to="/signup">
+                      Registrate
+                    </Link>
+                  </a>
+                </div>
+              </form>
+              <div>
+                <NotificationContainer />
+              </div>
+            </div>
+          )
+        }
       </AuthConsumer>
     );
   }
